194ecaa6d084d309d57e93a4a81d83ca835f2c3c
Mike Shenk <mlshenk415@gmail.com>
2020-10-05 14:07:43 -0700

    added linkedin button to Resume page and updated resume to React resume

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -13,14 +13,15 @@ function Resume() {
             <img className="img-fluid" id="biopic" src="./images/linkedin-headshot2020.jpg" alt="profilepic" />
           </div>
         </figure>
+        
         <figure className="row justify-content-center iframe-figure-div">
           <iframe title="resume" className="resume-window embed-responsive-item" src="./assets/mls-resume-react092520.pdf" width="100%" height="800px"></iframe>
         </figure>
         <figure className="row justify-content-center">
-          <a href="./assets/mike-shenk-tech-resume2020.pdf" target="_blank" className="resume-link"><strong>Click to view:</strong> Mike's Resume</a>
-        </figure><p>Click the link below to see more detail about my career on my LinkedIn page!</p>
+          <a href="./assets/mls-resume-react092520.pdf" target="_blank" className="resume-link"><strong>Click to view:</strong> Mike's Resume</a>
+        </figure>
         <a href="https://www.linkedin.com/in/michaelshenk415/">
-          <img className="sm-logos" src="./images/linkedinlogo.png" alt="linkedinlogo" target="_blank" />
+          <img className="linkedin justify-content-center" src="./images/linkedinlogo.png" alt="linkedinlogo" target="_blank" />
         </a>
       </section>
     </>
